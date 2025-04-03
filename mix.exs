2a0059eb2f5f1b349ca3a2222ff5031dc6234539
Johan van der Vleuten <jvdvleuten@gmail.com>
2025-04-03 17:28:06 +0200

    Fix warnings

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ICalendar.Mixfile do
       app: :icalendar,
       name: "ICalendar",
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.14",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -24,7 +24,7 @@ defmodule ICalendar.Mixfile do
 
   defp deps do
     [
-      {:timex, "~> 3.4"},
+      {:timex, github: "bitwalker/timex", branch: "main"},
       {:mix_test_watch, ">= 0.0.0", only: :dev},
       {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false}
     ]
